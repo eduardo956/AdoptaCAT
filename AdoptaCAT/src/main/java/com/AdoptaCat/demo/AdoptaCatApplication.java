ed1cc20a2c727f10fa05153cd42c7cbd4fd6d8c2
Eduardo Romero <74744910+eduardo956@users.noreply.github.com>
2022-05-29 21:22:41 -0500

    Archivos Iniciales

---
+++ AdoptaCAT/src/main/java/com/AdoptaCat/demo/AdoptaCatApplication.java
@@ -0,0 +1,13 @@
+package com.AdoptaCat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaCatApplication.class, args);
+	}
+
+}
---
+++ AdoptaCAT/src/test/java/com/AdoptaCat/demo/AdoptaCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AdoptaCat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptaCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
